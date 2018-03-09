2373a89d1ebaab85043d61a39830d523701ab8ee
Adriano Gil <adrianogil.san@gmail.com>
2018-03-08 20:07:33 -0400

    Added simple example of AndroidJavaClass

---
+++ android/AndroidPlugin/app/src/main/java/src/com/javaplugin/test/MyPlugin.java
@@ -0,0 +1,11 @@
+package com.javaplugin.test;
+
+import android.util.Log;
+
+public class MyPlugin
+{
+    public static void ButtonPressed()
+    {
+        Log.d("MyPlugin", "ButtonPressed");
+    }
+}
---
+++ unity/UnityPlugins/Assets/Scripts/JavaPlugin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JavaPlugin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        AndroidJavaClass jc
+            = new AndroidJavaClass (
+                "com.javaplugin.test.MyPlugin"
+        );
+
+        jc.CallStatic ("ButtonPressed");
+	}
+
+
+}
